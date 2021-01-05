c01b857435a2c0a082480f654675dbb26c0fcb0d
Yauhen Seliukou <sevgen87@gmail.com>
2021-01-05 19:07:17 +0300

    add to store parameters using localstore

--- mainJs.js
+++ mainJs.js
@@ -5,20 +5,38 @@ let monthInYear = ['января', 'февраля', 'марта', 'апреля
                    'мая', 'июня', 'июля', 'августа', 'сентября', 
                    'октября', 'ноября', 'декабря'];
 
-document.forms[0].lat.style.display = 'none';
-document.forms[0].lon.style.display = 'none';
-document.forms[0].country.style.display = 'none';
-document.forms[0].zipCode.style.display = 'none';
-let cityName = document.forms[0].cityName.value = 'Минск';
-let lat = document.forms[0].lat.value;
-let lon = document.forms[0].lon.value;
-let target = document.querySelector('.days').querySelector('li');
-let humidity = document.querySelector('.humidity').querySelector('input').checked;
-let wind = document.querySelector('.wind').querySelector('input').checked;
-let url = 'http://api.openweathermap.org/data/2.5/forecast?q='+ cityName + 
-              '&lang=ru&appid=3bca9c3f828ed0b6bfbbfe2affb63184';
+document.forms[0].cityName.style.display = (+localStorage.getItem('localCityVisible')) ? 'none' : '';
+document.forms[0].lat.style.display = (+localStorage.getItem('localLatVisible')) ? 'none' : '';
+document.forms[0].lon.style.display = (+localStorage.getItem('localLonVisible')) ? 'none' : '';
+document.forms[0].country.style.display = (+localStorage.getItem('localCountryVisible')) ? 'none' : '';
+document.forms[0].zipCode.style.display = (+localStorage.getItem('localZipCodeVisible')) ? 'none' : '';
 
-getWeatherDate(url, new Date(Date.now()));
+let cityName = document.forms[0].cityName.value = localStorage.getItem('localCity') 
+                                                ? localStorage.getItem('localCity') : 'Минск';
+
+document.forms[0].choice.value = localStorage.getItem('localChoiceSearch') 
+                                                ? localStorage.getItem('localChoiceSearch') : 'city';
+
+document.forms[0].lat.value = localStorage.getItem('localLatCoords') 
+                            ? (+localStorage.getItem('localLatCoords')) : '';
+document.forms[0].lon.value = localStorage.getItem('localLonCoords') 
+                            ? (+localStorage.getItem('localLonCoords')) : '';
+
+document.forms[0].country.value = localStorage.getItem('localCountryParam') 
+                                ? localStorage.getItem('localCountryParam') : '';
+document.forms[0].zipCode.value = localStorage.getItem('localZipCodeParam') 
+                                ? (+localStorage.getItem('localZipCodeParam')) : '';
+
+let target = localStorage.getItem('localDaysTarget') 
+            ? document.querySelector('.days').querySelector(`#${localStorage.getItem('localDaysTarget')}`) 
+            : document.querySelector('.days').querySelector('li');
+target.classList.add('active');
+
+document.forms[0].humidity.checked = (+localStorage.getItem('localHumidity')) ? true : false;
+document.forms[0].wind.checked = (+localStorage.getItem('localWind')) ? true : false;
+
+let url = createUrl();
+choiceDays(url);
 
 function getWeatherDate(url, day){
     fetch(url)
@@ -62,16 +80,15 @@ function getWeatherDate(url, day){
             newItemWeather.querySelector('.weatherIcon').innerHTML = `<img src="https://openweathermap.org/img/wn/${listItem.weather[0]['icon']}@2x.png">`;
             newItemWeather.querySelector('.weatherForecast').innerHTML = Math.round(listItem.main.temp - 273) + '&deg;';
             newItemWeather.querySelector('.weatherDesc').textContent = `На улице ${listItem.weather[0]['description']}`;
-            if (humidity) {
+            if (document.forms[0].humidity.checked) {
                 newItemWeather.querySelector('.weatherHumidity').textContent = `Влажность ${listItem.main.humidity} %`;
             }
-            if (wind) {
+            if (document.forms[0].wind.checked) {
                 newItemWeather.querySelector('.weatherWind').textContent = `Ветер ${listItem.wind.speed} км/ч`;
             }
         }
     })
       .catch(function (e) {
-        //if (e.name == 'TypeError') {
             switch (document.forms[0].choice.value) {
                 case 'city':
                     document.forms[0].cityName.classList.add('error');
@@ -88,10 +105,6 @@ function getWeatherDate(url, day){
             url = 'http://api.openweathermap.org/data/2.5/forecast?q=Minsk' + 
                   '&lang=ru&appid=3bca9c3f828ed0b6bfbbfe2affb63184';
             choiceDays(url);
-            //alert('Введены некорректные данные.');
-        /*} else {
-            throw e;
-        }*/
       });
 }
 
@@ -117,6 +130,31 @@ function choiceDays(url) {
     }
 }
 
+function createUrl() {
+    let newUrl;
+    switch (document.forms[0].choice.value) {
+        case 'city':
+            newUrl = 'http://api.openweathermap.org/data/2.5/forecast?q='
+                  + document.forms[0].cityName.value + 
+                  '&lang=ru&appid=3bca9c3f828ed0b6bfbbfe2affb63184';
+            break;
+        case 'coords':
+            newUrl = 'http://api.openweathermap.org/data/2.5/forecast?lat='
+                  + document.forms[0].lat.value + '&lon='
+                  + document.forms[0].lon.value +
+                  '&lang=ru&appid=3bca9c3f828ed0b6bfbbfe2affb63184';
+            break;
+        case 'zip':
+            if (document.forms[0].country.value === '') document.forms[0].country.value = 'us';
+            newUrl = 'http://api.openweathermap.org/data/2.5/forecast?zip='
+                  + document.forms[0].zipCode.value + ','
+                  + document.forms[0].country.value +
+                  '&lang=ru&appid=3bca9c3f828ed0b6bfbbfe2affb63184';
+            break;
+    }
+    return newUrl;
+}
+
 document.querySelector('.days').addEventListener('click', function(event){
     if (event.target.tagName != 'LI') return;
     
@@ -130,31 +168,7 @@ document.querySelector('.days').addEventListener('click', function(event){
 })
 
 document.querySelector('.buttonForm').addEventListener('click', function(event) {
-    let formParams = document.forms[0];
-    cityName = formParams.cityName.value;
-    humidity = document.querySelector('.humidity').querySelector('input').checked;
-    wind = document.querySelector('.wind').querySelector('input').checked;
-
-    switch (document.forms[0].choice.value) {
-        case 'city':
-            url = 'http://api.openweathermap.org/data/2.5/forecast?q='
-                  + document.forms[0].cityName.value + 
-                  '&lang=ru&appid=3bca9c3f828ed0b6bfbbfe2affb63184';
-            break;
-        case 'coords':
-            url = 'http://api.openweathermap.org/data/2.5/forecast?lat='
-                  + document.forms[0].lat.value + '&lon='
-                  + document.forms[0].lon.value +
-                  '&lang=ru&appid=3bca9c3f828ed0b6bfbbfe2affb63184';
-            break;
-        case 'zip':
-            if (document.forms[0].country.value === '') document.forms[0].country.value = 'us';
-            url = 'http://api.openweathermap.org/data/2.5/forecast?zip='
-                  + document.forms[0].zipCode.value + ','
-                  + document.forms[0].country.value +
-                  '&lang=ru&appid=3bca9c3f828ed0b6bfbbfe2affb63184';
-            break;
-    }
+    url = createUrl();
     choiceDays(url);
 })
 
@@ -187,6 +201,8 @@ document.forms[0].choice.addEventListener('click', function(event) {
             document.forms[0].zipCode.style.display = '';
             break;
     }
+
+    localStorage.setItem('localChoiceSearch', document.forms[0].choice.value);
 })
 
 document.forms[0].cityName.oninput = function() {
@@ -200,3 +216,22 @@ document.forms[0].country.oninput = document.forms[0].zipCode.oninput = function
     document.forms[0].country.classList.remove('error');
     document.forms[0].zipCode.classList.remove('error');
 }
+
+window.addEventListener('beforeunload', function() {
+    localStorage.setItem('localDaysTarget', target.id);
+
+    localStorage.setItem('localCity', document.forms[0].cityName.value);
+    localStorage.setItem('localHumidity', document.forms[0].humidity.checked ? 1 : 0);
+    localStorage.setItem('localWind', document.forms[0].wind.checked  ? 1 : 0);
+
+    localStorage.setItem('localCityVisible', document.forms[0].cityName.style.display ? 1 : 0);
+    localStorage.setItem('localLatVisible', document.forms[0].lat.style.display ? 1 : 0);
+    localStorage.setItem('localLonVisible', document.forms[0].lon.style.display ? 1 : 0);
+    localStorage.setItem('localCountryVisible', document.forms[0].country.style.display ? 1 : 0);
+    localStorage.setItem('localZipCodeVisible', document.forms[0].zipCode.style.display ? 1 : 0);
+
+    localStorage.setItem('localLatCoords', document.forms[0].lat.value);
+    localStorage.setItem('localLonCoords', document.forms[0].lon.value);
+    localStorage.setItem('localZipCodeParam', document.forms[0].zipCode.value);
+    localStorage.setItem('localCountryParam', document.forms[0].country.value);
+})
