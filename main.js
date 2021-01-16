fc8e5b5c0b86205cd847ffce0c3632ebd4ac25c7
Yauhen Seliukou <sevgen87@gmail.com>
2021-01-16 13:53:12 +0300

    add styles for tablets and mobile phones, fixed main.js

--- main.js
+++ main.js
@@ -1,63 +1,107 @@
-
 initialDataRecording(JSON.parse(localStorage.getItem('dataForm')));
 
-document.querySelector('.days').addEventListener('click', function(event){
+document.querySelector('.days').addEventListener('click', (event) => {
     if (event.target.tagName != 'LI') return;
     
-    let days = document.querySelector('.days .active');
-    days.classList.remove('active');
-    days = event.target;
-    days.classList.add('active');
+    document.querySelector('.days .active').classList.remove('active');
+    event.target.classList.add('active');
     
-    showWeather(getUrl());
+    addWeatherInfoInApp(getUrl());
 });
 
-document.querySelector('.button-submit-weather-params').addEventListener('submit', () => showWeather(getUrl()));
+document.querySelector('.submit-weather-params-button').addEventListener('submit', () => addWeatherInfoInApp(getUrl()));
 
-document.querySelector('.header-title').addEventListener('click', function() {
-    const sidebar = document.querySelector('.sidebar');
-    /*if (sidebar.classList.contains('open-sidebar')) {
-        removeClass('open-sidebar', sidebar);
-        addClass('close-sidebar', sidebar);
-    } else {
-        removeClass('close-sidebar', sidebar);
-        addClass('open-sidebar', sidebar)
-    }*/
-    sidebar.hidden = !sidebar.hidden;
+document.querySelector('.clear-weather-params-button').addEventListener('click', (event) => {
+    event.preventDefault();
+    const { 
+        lat, 
+        lon, 
+        country,
+        humidity, 
+        wind,
+        'city-name': city, 
+        'zip-code': zipCode, 
+        'select-weather-by-param': selectParams 
+    } = document.forms['weather-filters'];
+
+        selectParams.value = 'city';
+        city.value = '';
+        lat.value = '';
+        lon.value = '';
+        country.value = '';
+        zipCode.value = '';
+        humidity.checked = false;
+        wind.checked = false;
+        localStorage.clear();
+        showParamsWeatherBySelect();
 });
 
-document.querySelector('.select-weather-param').addEventListener('change', () => showParamsWeatherBySelect());
+document.querySelector('.header-title').addEventListener('click', () => {
+    if (document.documentElement.clientWidth < 750) {
+        const sidebar = document.querySelector('.sidebar');
+        const weatherFilters = document.forms['weather-filters'];
+        const content = document.querySelector('.content');
+        if (sidebar.hidden) {
+            sidebar.hidden = !sidebar.hidden;
+            setTimeout(() => {
+                weatherFilters.classList.remove('close-sidebar');
+                weatherFilters.classList.add('open-sidebar');
+                content.classList.remove('run-content-at-close-sidebar');
+                content.classList.add('run-content-at-open-sidebar');
+            }, 200);
+        } else {
+            weatherFilters.classList.add('close-sidebar');
+            weatherFilters.classList.remove('open-sidebar');
+            content.classList.add('run-content-at-close-sidebar');
+            content.classList.remove('run-content-at-open-sidebar');
+            setTimeout(() => {
+                sidebar.hidden = !sidebar.hidden;
+            }, 1000);
+        }
+    }
+});
 
-document.querySelector('.city-name').oninput = function() {
+document.querySelector('.select-weather-by-param').addEventListener('change', () => showParamsWeatherBySelect());
+
+document.querySelector('.city-name').oninput = () => {
     const city = document.querySelector('.city-name');
     removeClass('error', city);
 };
 
-document.querySelector('.lat').oninput = document.querySelector('.lon').oninput = function() {
-    const {lat, lon} = document.forms['weather-params'];
+document.querySelector('.lat').oninput = document.querySelector('.lon').oninput = () => {
+    const {
+        lat, 
+        lon
+    } = document.forms['weather-filters'];
     removeClass('error', lat, lon);
 };
 
-document.querySelector('.country').oninput = document.querySelector('.zip-code').oninput = function() {
-    const {country, 'zip-code': zipCode} = document.forms['weather-params'];
+document.querySelector('.country').oninput = document.querySelector('.zip-code').oninput = () => {
+    const {
+        country, 
+        'zip-code': zipCode
+    } = document.forms['weather-filters'];
     removeClass('error', country, zipCode);
 };
 
-window.addEventListener('beforeunload', function() {
+window.addEventListener('beforeunload', () => {
     localStorage.setItem('dataForm', JSON.stringify(returnDataForm()));
 });
 
 function initialDataRecording(localData) {
-    let days;
-    const {'city-name': city, lat, lon, country, 
-           'zip-code': zipCode, 'select-weather-param': selectParams,
-            humidity, wind} = document.forms['weather-params'];
+    const {
+        lat, 
+        lon, 
+        country, 
+        humidity, 
+        wind,
+        'city-name': city, 
+        'zip-code': zipCode, 
+        'select-weather-by-param': selectParams
+    } = document.forms['weather-filters'];
 
     if (localData) {
         switch (localData.selectParams) {
-            case 'city':
-                city.value = localData.city;
-                break;
             case 'coords':
                 lat.value = localData.lat;
                 lon.value = localData.lon;
@@ -67,27 +111,31 @@ function initialDataRecording(localData) {
                 zipCode.value = localData.zipCode;
                 break;
         }
-        selectParams.value = localData.selectParams;
         humidity.checked = localData.humidity;
         wind.checked = localData.wind;
-        days = document.querySelector(`.days #${localData.days}`);
-    } else {
-        document.forms['weather-params']['city-name'].value = 'Минск';
-        document.forms['weather-params']['select-weather-param'].value = 'city';
-        days = document.querySelector('.days li');
     }
 
-    showParamsWeatherBySelect()
+    selectParams.value = localData ? localData.selectParams : 'city';
+    city.value = localData ? localData.city : 'Minsk';
     
-    days.classList.add('active');
+    document.querySelector(`.days ${localData ? "#" + localData.days : "li"}`).classList.add('active');
 
-    showWeather(getUrl());
+    showParamsWeatherBySelect();
+    addWeatherInfoInApp(getUrl());
 };
 
 function returnDataForm() {
-    const {'city-name': city, lat, lon, country, 
-           'zip-code': zipCode, 'select-weather-param': selectParams,
-            humidity, wind} = document.forms['weather-params'];
+    const { 
+        lat, 
+        lon, 
+        country,
+        humidity, 
+        wind,
+        'city-name': city, 
+        'zip-code': zipCode, 
+        'select-weather-by-param': selectParams 
+    } = document.forms['weather-filters'];
+    
     const dataForm = {
         city: city.value,
         lat: lat.value,
@@ -103,59 +151,73 @@ function returnDataForm() {
     return dataForm;
 };
 
-function createHtmlContentForOneDay(responseData, dateWeather) {
+function addHtmlContentToPage(responseData) {
     const options = { 
         weekday: 'long',
         year: 'numeric',
         month: 'long',
         day: 'numeric' 
     };
-    const formatDate = new Intl.DateTimeFormat('ru', options).format(dateWeather);
 
-    const {humidity, wind} = document.forms['weather-params'];
+    const selectDay = document.querySelector('.days .active').id;
+    const nowDate = new Date();
+    const currentDate = (selectDay == 'today') ? nowDate :
+                        (selectDay == 'tomorrow') ? new Date(nowDate.getTime() + (24 * 60 * 60 * 1000)) : '';
+
+    const {
+        humidity, 
+        wind
+    } = document.forms['weather-filters'];
+
+    let itemWeather = '';
     
-    const html = responseData.list.reduce((text, oneDayData) => {
+    document.querySelector('.content').innerHTML = responseData.list.reduce((text, oneDayData) => {
         const time = oneDayData.dt_txt.split(' ')[1].split(':');
-        const date = new Date(oneDayData.dt_txt.split(' ')[0].split('-').join(','));
+        const date = new Date(oneDayData.dt_txt);
         
-        if (dateWeather.getDate() == date.getDate() && time[0] >= '09') {
-            text +=
+        if ((selectDay == 'week') ? true : currentDate.getDate() === date.getDate()) {
+            itemWeather +=
                 `<div class="weather-item">
                     <p class="weather-time">${time[0]}:${time[1]}</p>
                     <p class="weather-forecast">${Math.round(oneDayData.main.temp - 273) + '&deg;'}</p>
                     <p class="weather-icon"><img src="https://openweathermap.org/img/wn/${oneDayData.weather[0]['icon']}@2x.png"></p>
                     <div class="weather-info">
-                        <p class="weather-desc">На улице ${oneDayData.weather[0]['description']}</p>
-                        <p class="weather-humidity">${humidity.checked ? `Влажность ${oneDayData.main.humidity}%` : ''}</p>
-                        <p class="weather-wind">${wind.checked ? `Ветер: ${oneDayData.wind.speed} км/ч` : ''}</p>
+                        <p class="weather-desc">Outdoors ${oneDayData.weather[0]['description']}</p>
+                        <p class="weather-humidity">${humidity.checked ? `Humidity ${oneDayData.main.humidity}%` : ''}</p>
+                        <p class="weather-wind">${wind.checked ? `Wind speed: ${oneDayData.wind.speed} km/h` : ''}</p>
                     </div>
                 </div>`;
         }
+        if (time[0] == '21') {
+            const formatDate = new Intl.DateTimeFormat('en', options).format(date);
+            text += itemWeather ?
+                `<div class="one-day">
+                    <h2 class="weather-title">${responseData.city.name}: ${formatDate}</h2>
+                    <div class="weather-container">${itemWeather}</div></div>`
+                : '';
+            itemWeather = '';
+        }
         return text;
     }, '');
-
-    return `<div class="one-day">
-            <h2 class="weather-title">${responseData.city.name}: ${formatDate}</h2>
-            <div class="weather-container">${html}</div></div>`;
 };
 
-function addInfoInApp(url, dateWeather, numberOfDays){
+function addWeatherInfoInApp(url){
     fetch(url).then(function(resp) {
         return resp.json() 
     })
     .then(function(data) {
-        let html = ''
-        for (let i = 0; i < numberOfDays; i++) {
-            const currentDate = new Date(dateWeather.getFullYear(), dateWeather.getMonth(), dateWeather.getDate() + i);
-            html += createHtmlContentForOneDay(data, currentDate);
-        }
-        document.querySelector('.content').innerHTML = html;
+        addHtmlContentToPage(data);
     })
     .catch(function (e) {
         console.log(e);
-        const {'city-name': city, lat, lon, country, 
-               'zip-code': zipCode, 'select-weather-param': selectParams,
-                humidity, wind} = document.forms['weather-params'];
+        const { 
+            lat, 
+            lon, 
+            country,
+            'city-name': city, 
+            'zip-code': zipCode, 
+            'select-weather-by-param': selectParams 
+        } = document.forms['weather-filters'];
 
         switch (selectParams.value) {
             case 'city':
@@ -173,47 +235,43 @@ function addInfoInApp(url, dateWeather, numberOfDays){
     });
 };
 
-function showWeather(url) {
-    const dateNow = new Date();
-    switch (document.querySelector('.days .active').id) {
-        case 'today':
-            addInfoInApp(url, dateNow, 1); 
-            break;
-        case 'tomorrow':
-            const dateTommorow = new Date(dateNow.getFullYear(), dateNow.getMonth(), dateNow.getDate() + 1);
-            addInfoInApp(url, dateTommorow, 1);
-            break;
-        case 'week':
-            addInfoInApp(url, dateNow, 5);
-            break;
-    }
-};
-
 function getUrl() {
-    const {'city-name': city, lat, lon, country, 
-           'zip-code': zipCode, 'select-weather-param': selectParams} = document.forms['weather-params'];
+    const { 
+        lat, 
+        lon, 
+        country,
+        'city-name': city, 
+        'zip-code': zipCode, 
+        'select-weather-by-param': selectParams 
+    } = document.forms['weather-filters'];
 
     switch (selectParams.value) {
         case 'city': 
-            return (`http://api.openweathermap.org/data/2.5/forecast?q=${city.value}&lang=ru&appid=80fceb75a14d5106f197a58ff38572dc`);
+            return (`http://api.openweathermap.org/data/2.5/forecast?q=${city.value}&lang=en&appid=80fceb75a14d5106f197a58ff38572dc`);
         case 'coords': 
-            return (`http://api.openweathermap.org/data/2.5/forecast?lat=${lat.value}&lon=${lon.value}&lang=ru&appid=80fceb75a14d5106f197a58ff38572dc`);
+            return (`http://api.openweathermap.org/data/2.5/forecast?lat=${lat.value}&lon=${lon.value}&lang=en&appid=80fceb75a14d5106f197a58ff38572dc`);
         case 'zip': 
-            return (`http://api.openweathermap.org/data/2.5/forecast?zip=${zipCode.value},${country.value}&lang=ru&appid=80fceb75a14d5106f197a58ff38572dc`);
+            return (`http://api.openweathermap.org/data/2.5/forecast?zip=${zipCode.value},${country.value}&lang=en&appid=80fceb75a14d5106f197a58ff38572dc`);
     }
 };
 
 function addClass(name, ...elems) {
-    elems.map((elem) => elem.classList.add(name));
+    elems.forEach((elem) => elem.classList.add(name));
 };
 
 function removeClass(name, ...elems){
-    elems.map((elem) => elem.classList.remove(name));
+    elems.forEach((elem) => elem.classList.remove(name));
 };
 
 function showParamsWeatherBySelect() {
-    const {'city-name': city, lat, lon, country, 
-           'zip-code': zipCode, 'select-weather-param': selectParams} = document.forms['weather-params'];
+    const { 
+        lat, 
+        lon, 
+        country,
+        'city-name': city, 
+        'zip-code': zipCode, 
+        'select-weather-by-param': selectParams 
+    } = document.forms['weather-filters'];
 
     switch (selectParams.value) {
         case 'city':
